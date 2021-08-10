1955a6eade36331c6d09380951edf66865b4f88e
Ђорђе <37450617+Djolevil@users.noreply.github.com>
2021-08-10 16:39:51 +0200

    Add AddMovie component and its styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -12,14 +13,14 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-    const response = await fetch('https://swapi.dev/api/films/');
-    if (!response.ok){
-      throw new Error('Something went wrong!');
-    }
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
-    const data = await response.json();
+      const data = await response.json();
 
-    const transformedMovies = data.results.map(movieData => {
+      const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
@@ -28,7 +29,7 @@ function App() {
         };
       });
       setMovies(transformedMovies);
-    } catch(error){
+    } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
@@ -38,26 +39,33 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
+
   if (error) {
     content = <p>{error}</p>;
   }
+
   if (isLoading) {
-    content = <p>Loading..</p>;
+    content = <p>Loading...</p>;
   }
-  
+
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
